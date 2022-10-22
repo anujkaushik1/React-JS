c869212fc96313dc8c04031da184f8f5710c7564
Anuj Kaushik <anujkaushik1512@gmail.com>
2022-10-22 16:20:17 +0530

    Created functions and class components

--- React/todo/src/App.js
+++ React/todo/src/App.js
@@ -1,7 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
 import Todo from './Components/Todo';
-
 function App() {
   return (
       <Todo/>
--- React/todo/src/Components/Todo.js
+++ React/todo/src/Components/Todo.js
@@ -1,11 +1,48 @@
 import React, { Component } from 'react'
 
 export default class Todo extends Component {
+
+    constructor(){
+        super();
+        this.state = {
+            tasks : [{task : 'Check mails', id : 1}, {task : 'Read article', id : 2}, {task : 'Do development', id : 3}],
+            currTask : ''
+        }
+    }
+
+    handleChange = (e) => {
+        let input = e.target.value;  
+        this.setState({
+            currTask : input
+        })
+    }
+
+    handleSubmit = () => {
+        // let oldTask = [...this.state.tasks];
+
+        this.setState({
+            tasks : [...this.state.tasks , {task : this.state.currTask, id : this.state.tasks.length + 1}]
+        })
+    }
+
   render() {
     return (
       <div>
-        Hello World !
+            <input type="text" value={this.state.currTask} onChange={this.handleChange}/>
+            <button onClick={this.handleSubmit}>Submit</button>
+            <ul>
+            {
+                this.state.tasks.map((taskObj) => (
+                    <li key={taskObj.id}>
+                        <p>{taskObj.task}</p>
+                        <button>Delete</button>
+                    </li>
+                ))
+            }
+            </ul>
+
       </div>
     )
   }
 }
+
